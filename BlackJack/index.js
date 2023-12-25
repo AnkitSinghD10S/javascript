b80f69329181595f3df8b488d411dd3a72d7d582
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2023-12-25 21:54:21 +0530

    BlackJack

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards =[firstCard , secondCard]// arrays of card 
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -8,7 +9,13 @@ let messageEl = document.getElementById("message-el")
 console.log(messageEl);
 // let sumEl=document.getElementById("sum-el")
 let sumEl =document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
+
 function startGame(){
+    renderGame()
+}
+function renderGame(){
+    cardsEl.textContent= "Cards:" + cards[0] + " " + cards[1]
     sumEl.textContent="sum:" + sum
 if (sum <= 20) {
     
@@ -25,5 +32,11 @@ if (sum <= 20) {
     
 }
 messageEl.textContent = message;
+}
+function newCard(){
+    console.log("Drawing a new card from the deck!")
+    let card = 5
+    sum+=card
+    renderGame();
 }
 
---
+++ practiceJs/arrays.js
@@ -0,0 +1,23 @@
+//  Arrays - ordered lists of items
+ let featuredPosts = [
+    "Check out my Netflix clone",
+ "Here's the code for my project",
+ "I've just relaunched my portfolio"
+]
+let mySelf =['ankit','b.tech','19 years old']
+console.log(featuredPosts)
+console.log(mySelf)
+// arrays are 0 indexed
+console.log(mySelf[0])
+
+// arrays is a complex data type it uses all the primitive and objects can be stored in it
+
+let arrys=["ankit ", 24 , true]
+
+// we can add element to array using push
+let card=[4,7]
+card.push(9)
+console.log(card)
+let a =card.pop()
+console.log(card)
+console.log(a)
