d8188f48322d8415fc7230fe4ddb0b0dfc81cb0d
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2023-12-26 14:39:16 +0530

    practice

--- practiceJs/ifelse.js
+++ practiceJs/ifelse.js
@@ -1,19 +1,61 @@
 // Check if the person is elegible for a birthday card from the King! (100)
 
-let age = 100
+// let age = 100
 
-// if less than 100    -> "Not elegible"
-// else if exactly 100 -> "Here is your birthday card from the King!"
-// else                -> "Not elegible, you have already gotten one"
-if(age<100){
-    console.log("Not elegible");
-}
-else if(age===100){
+// // if less than 100    -> "Not elegible"
+// // else if exactly 100 -> "Here is your birthday card from the King!"
+// // else                -> "Not elegible, you have already gotten one"
+// if(age<100){
+//     console.log("Not elegible");
+// }
+// else if(age===100){
 
-    console.log("Here is your birthday card from the King!");
-}
-else{
-    console.log("Not elegible, you have already gotten one");
-}
+//     console.log("Here is your birthday card from the King!");
+// }
+// else{
+//     console.log("Not elegible, you have already gotten one");
+// }
 
-// the diffrence between '==' and '===' is that it double equals take bth string and number equal to the value but === takes of that value to be equal 
+// // the diffrence between '==' and '===' is that it double equals take bth string and number equal to the value but === takes of that value to be equal 
+
+
+let age = 15
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+// if(age<6){
+//     console.log("free")
+// }
+// else if(age>=6||age<=17){
+//     console.log('child discount')
+// }
+// else if(age>=18||age<=26){
+//     console.log('student discount')
+// }
+// else if(age>=27||age<=66){
+//     console.log('full price')
+// }
+// else {
+//     console.log('senior citizen discount')
+// }
+
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+for(let i=0;i<largeCountries.length;i++){
+    console.log(largeCountries[i])
+}
