eb0ed09af7d4f8ac80af0dc06b04033042705efa
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2023-12-29 23:09:08 +0530

    a

--- practiceJs/ifelse.js
+++ practiceJs/ifelse.js
@@ -60,17 +60,17 @@ let age = 15
 //     console.log(largeCountries[i])
 // }
 
-let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+// let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
 
-// You need to help me fixup the largeCountries array so that 
-// China and Pakistan are added back into their respective places
+// // You need to help me fixup the largeCountries array so that 
+// // China and Pakistan are added back into their respective places
 
-// Use push() & pop() and their counterparts unshift() & shift()
-// Google how to use unshift() and shift()
+// // Use push() & pop() and their counterparts unshift() & shift()
+// // Google how to use unshift() and shift()
 
-largeCountries.pop()
-largeCountries.push("pakistan")
-largeCountries.shift()
-largeCountries.unshift("China")
-console.log(largeCountries)
+// largeCountries.pop()
+// largeCountries.push("pakistan")
+// largeCountries.shift()
+// largeCountries.unshift("China")
+// console.log(largeCountries)
 
