ac9b135dfd1d0529a0b3d1b617b93456d77288d9
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2023-12-29 23:53:29 +0530

    fruitsSort

--- practiceJs/loops.js
+++ practiceJs/loops.js
@@ -36,11 +36,11 @@
 //     console.log(cards[i])
 // }
 
-let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
-let greetingEl = document.getElementById("greeting-el")
+// let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
+// let greetingEl = document.getElementById("greeting-el")
 
-// Render the sentence in the greetingEl paragraph using a for loop and .textContent
+// // Render the sentence in the greetingEl paragraph using a for loop and .textContent
 
-for(let i=0;i<sentence.length;i++){
-    greetingEl.textContent+=sentence[i]
-}
+// for(let i=0;i<sentence.length;i++){
+//     greetingEl.textContent+=sentence[i]
+// }
