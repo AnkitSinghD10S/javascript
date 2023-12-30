32552bc27786d609d4c6cba884d1dde98609f783
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2023-12-30 23:12:31 +0530

    a

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,3 +1,10 @@
-function saveLead(){
-    console.log("save input")
-}
+
+const inputBtn=document.getElementById("input-btn")
+
+inputBtn.addEventListener("click",function(){
+    console.log("button clicked from EventListner")
+})
+// here "click" sepcify the event and the function is define which is to be envoked
+
+let myLeads = []
+const inputEL = document.getElementById("input-el")
