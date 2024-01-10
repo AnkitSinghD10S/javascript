7ba98793f8f57d1f9f381ad9906b6071bb182d8f
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-01-10 21:01:39 +0530

    a

--- practiceJs/templetstring.js
+++ practiceJs/templetstring.js
@@ -4,7 +4,16 @@ const recipient = "James"
 
 // Refactor the email string to use template strings
 // const email = "Hey " + recipient + "! How is it going? Cheers Per"
-const email = `Hey ${recipient  }! How is it going? Cheers Per`
+
+// Create a new variable, sender, and set its value to your name
+let sender="ankit"
+// Use your sender variable instead of "Per"
+
+// Break the email string into multiple lines
+const email = `Hey ${recipient  }
+! How is it going?
+Cheers ${sender}
+`
 
 
 console.log(email)
