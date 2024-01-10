25aee9e88d2235bcdd61bb1450a9a1ea7c8be638
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-01-10 21:36:55 +0530

    lacalstorage

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -3,6 +3,10 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+localStorage.setItem("myleads","www.leads.com")
+console.log( localStorage.getItem("myLeads") ) 
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
