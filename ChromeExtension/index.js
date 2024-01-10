d508691d252639e9b09734e9fcde6c6020fe5eda
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-01-10 21:18:50 +0530

    date

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,38 +1,24 @@
-
-// here "click" sepcify the event and the function is define which is to be envoked
-
 let myLeads = []
 const inputEl = document.getElementById("input-el")
-const inputBtn=document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-inputBtn.addEventListener("click",function(){
+
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    inputEl.value = '';
-    renderLead()
+    inputEl.value = ""
+    renderLeads()
 })
 
-function renderLead(){
-let listItems=""
-for(let i=0;i<myLeads.length;i++){
-//ulEl.innerHTML+="<li>"+myLeads[i]+"</li>"
-// create element
-// set text content
-// append to ul
-// const li =document.createElement("li")
-// li.textContent = myLeads[i]
-// ulEl.append(li)
-
-    // listItems += "<li><a href = '"+myLeads[i]+"' target='_blank'>"+myLeads[i]+"</a>"+"</li>"
-
-    // templet strings
-    listItems +=`
-    <li>
-        <a href = "${myLeads[i]}" target='_blank'>
-            ${myLeads[i]}
-        </a>
-    </li>
-    `
-    
-}
-ulEl.innerHTML = listItems
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
