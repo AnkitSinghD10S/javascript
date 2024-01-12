aa2bab8a7ff4a2105ccb2fcdaa81d5b4a958428d
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-01-12 23:42:44 +0530

    saveLeads

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -4,22 +4,31 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteAll = document.getElementById("delete-all")
+const tabBtn= document.getElementById("tab-btn")
 // localStorage.setItem("myleads","www.leads.com")
 // console.log( localStorage.getItem("myLeads") ) 
-
 // localStorage.clear() 
 
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 // console.log(leadsFromLocalStorage)
 
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click",function(){
+    // console.log("clicked")
+    // console.log(tabs[0].url)
+    myLeads.push(tabs[0].url) 
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+})
+
 if(leadsFromLocalStorage){
     myLeads=leadsFromLocalStorage
     render(myLeads)
 }
-    
-\
 
- 
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
