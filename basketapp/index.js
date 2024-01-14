a073cdbf8cce9e16153865d395153c3745f66706
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-01-14 14:20:09 +0530

    basketapp

--- basketapp/index.js
+++ basketapp/index.js
@@ -1,8 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref , push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+const appSettings = {
+    databaseURL:"https://playground-ff712-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase (app)
+const shoppingListInDB = ref(database , "shoppingList")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingList = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    push(shoppingListInDB, inputValue)
     console.log(inputValue)
+    render(fruits)
 })
+
+function render (list){
+    let innerList=""
+    for(let i=0;i<list.length;i++){
+        innerList+=`
+        <li> ${list[i]} </li>
+        `
+    }
+    shoppingList.innerHTML = innerList
+}
+let fruits =["oranges", "banana"] 
